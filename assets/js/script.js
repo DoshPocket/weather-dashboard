eb0ad71372dcbada1ea1c13f3fffd2e4674aad19
Kenneth MacLean <kenneth.maclean84@gmail.com>
2022-01-31 21:29:10 -0500

    added local storage, history and clear history button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+function init(){
+
 let fetchButton = document.getElementById('fetch-button');
 let currentWeatherEl = document.getElementById('current-weather')
 let cityEl = document.getElementById("enter-city-name");
@@ -12,13 +14,17 @@ let historyEl = document.getElementById("history");
 let clearEl = document.getElementById("clear-history");
 let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
-
-
 const apiKey = "3ec9ebfa550c41947096967b17d132e7";
 
 function getApi(event) {
   event.preventDefault();
   let cityName = cityEl.value.trim();
+        let searchTerm = cityEl.value;
+      // getApi(searchTerm);
+      searchHistory.push(searchTerm);
+      console.log("1");
+      localStorage.setItem("search", JSON.stringify(searchHistory));
+      renderSearchHistory();
   let requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + apiKey;
   // retrieves data from openweather for user inputted city
   fetch(requestUrl).then(function (response) {
@@ -28,11 +34,12 @@ function getApi(event) {
       .then(function (data) {
         console.log(data);
         currentWeatherEl.classList.remove("d-none");
-
-        const currentDate = moment().format("MMMM, Do, YYYY");
-        nameEl.innerHTML = data.name + currentDate;
-        let weatherPic = data.weather[0].icon;
-        currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+        // current day weather
+        let currentDate = moment().format("MMMM, Do, YYYY");
+        nameEl.innerHTML = data.name + " " +currentDate;
+        let currentWeatherIcon = data.weather[0].icon;
+        // let currentPicEl = document.createElement("img");
+        currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png");
         currentPicEl.setAttribute("alt", data.weather[0].description);
         currentTempEl.innerHTML = "Temperature: " + data.main.temp + " &#176C";
         currentHumidityEl.innerHTML = "Humidity: " + data.main.humidity + "%";
@@ -42,6 +49,7 @@ function getApi(event) {
         let lon = data.coord.lon;
         let uvRequestUrl =
           "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&cnt=1";
+
         fetch(uvRequestUrl)
         .then(function (response) {
           return response.json()
@@ -76,9 +84,7 @@ function getApi(event) {
             let forecastEl = document.querySelectorAll(".forecast");
             for (i = 0; i < forecastEl.length; i++) {
               forecastEl[i].innerHTML = "";
-              //  create card for 5-day forecast
-              //  let forecastIcon = 'https://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png'
-              // console.log(forecastIcon)
+              //  5-day forecast display
               let forecastDate = moment().add(i + 1, "days").format("MMMM/DD/YYYY");
               let forecastTemp = data.list[i].main.temp + " &#176C"
               let forecastWind = data.list[i].wind.speed + " m/s"
@@ -88,6 +94,11 @@ function getApi(event) {
               forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
               forecastDateEl.innerHTML = forecastDate;
               forecastEl[i].append(forecastDateEl);
+
+              let forecastWeatherEl = document.createElement("img");
+              forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png");
+              forecastWeatherEl.setAttribute("alt", data.list[i].weather[0].description);
+              forecastEl[i].append(forecastWeatherEl);
 
               let forecastTempEl = document.createElement("p");
               forecastTempEl.innerHTML = ("Temp: ") + forecastTemp;
@@ -103,8 +114,32 @@ function getApi(event) {
             }
           });
         });
+  // Clear History button
+  clearEl.addEventListener("click", function () {
+      localStorage.clear();
+      searchHistory = [];
+      renderSearchHistory();
+          })
       });
   });
 };
 
+function renderSearchHistory() {
+  historyEl.innerHTML = "";
+  for (let i = 0; i < searchHistory.length; i++) {
+      let historyItem = document.createElement("input");
+      historyItem.setAttribute("type", "text");
+      historyItem.setAttribute("readonly", true);
+      historyItem.setAttribute("class", "form-control d-block bg-white");
+      historyItem.setAttribute("value", searchHistory[i]);
+      historyItem.addEventListener("click", function () {
+      })
+      historyEl.append(historyItem);
+  }
+}
+
 fetchButton.addEventListener('click', getApi);
+
+}
+
+init()
